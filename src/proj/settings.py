f174448437885322e068976882f25f85549472b9
SergelAleksandr <64100564+SergelAleksandr@users.noreply.github.com>
2020-06-14 20:49:17 +0300

    Update settings.py

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,12 +25,9 @@ SECRET_KEY = 'n4jjt7xvw-#^$b+l*axkk42s*0_03^wnu8v&j7*19by$8+%2g&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
+
 ALLOWED_HOSTS = ['sergalnik.pythonanywhere.com']
-# 'http://sergalnik.pythonanywhere.com/list_genre/'
-=======
-ALLOWED_HOSTS = ['sergalnik.pythonanywhere.com']
->>>>>>> 3473e2c82dbacc58e16c687dcdafb3b0e3652da6
+
 
 
 # Application definition
